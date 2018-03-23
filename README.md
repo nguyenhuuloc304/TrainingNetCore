9afe2578b01ac6ca13e685ff93abe1e3ff37b603
ORIENTSOFTWARE\nhloc <nguyenhuuloc304@gmail.com>
2018-03-23 10:09:45 +0700

    update code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TrainingNetCore
+
+https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/powershell
+
+https://docs.microsoft.com/en-us/ef/core/
+
+Migration
+https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/
---
+++ Tranining/Training.EFCore/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace Training.EFCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //var connection = @"Data Source=.;Initial Catalog=Interns;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
