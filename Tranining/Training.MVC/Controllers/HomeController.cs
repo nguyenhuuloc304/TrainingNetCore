fe6f7a6084431240e38ebaa49aa8dd8e25902d8e
ORIENTSOFTWARE\nhloc <nguyenhuuloc304@gmail.com>
2018-03-27 15:29:09 +0700

    Update code for MVC, Identity

---
+++ Tranining/Training.MVC/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Training.MVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Training.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //await _roleManager.CreateAsync(new IdentityRole { Name = "User" });
+            //_roleManager.CreateAsync(new IdentityRole { Name = "User" }).Result;
+            var userAdmin = await _userManager.FindByEmailAsync("nguyenhuuloc304@gmail.com");
+            await _userManager.AddToRoleAsync(userAdmin, "Admin");
+
+            var user = await _userManager.FindByEmailAsync("nguyenhuuloc3041@gmail.com");
+            await _userManager.AddToRoleAsync(user, "User");
+
+            return View();
+        }
+
+        [Authorize(Roles = "User")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
